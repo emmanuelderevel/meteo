1b3db5127d3f9fdd43e58d613ab03ee3a5305237
Emmanuel de Revel <emmanuel.de-revel@student.ecp.fr>
2017-11-02 15:27:40 +0100

    fusion

--- city/weather.py
+++ city/weather.py
@@ -8,21 +8,29 @@ class Weather():
         self.lon=''
         self.lat=''
 
-    def retriveWeathInfo(self):
+    def retrieveWeathInfo(self):
         html = 'http://api.openweathermap.org/data/2.5/weather?' \
                'id={}&units=metric&APPID=955716c6c3b27005023580d9021147ba'.format(self.city_id)
         client = requests.get(html)
         j = client.json()
         self.dict['temp'] = j['main']['temp']
+        self.dict['temp_max'] = j['main']['temp_max']
+        self.dict['temp_min'] = j['main']['temp_min']
         self.dict['main'] = j['weather'][0]['main']
+        self.dict['icon'] = j['weather'][0]['icon']
         self.dict['description'] = j['weather'][0]['description']
         self.dict['humidity'] = j['main']['humidity']
         self.dict['pressure'] = j['main']['pressure']
         self.dict['wind'] = j['wind']['speed']
-        self.lon = j['coord']['lon']
-        self.lat = j['coord']['lat']
-        #self.dtime = j
+        self.dict['city'] = j['name']
+        self.dict['lon'] = j['coord']['lon']
+        self.dict['lat'] = j['coord']['lat']
 
     def __str__(self):
         return 'Informations météorologiques de la ville de Londre : {}'.format(self.dict)
 
+
+if __name__ == '__main__':
+    p=Weather(2362344)
+    p.retrieveWeathInfo()
+    print(p.dict)
